dad883d737568662ebe67c8ddc8e7cd3e9e52bad
Nikolay <koddek@bk.ru>
2024-02-14 12:28:05 +0300

    project setup

--- Registration/ViewController/ViewController.swift
+++ Registration/ViewController/ViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
 
 }
 
