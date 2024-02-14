3b6ea25528e253f72d6260e8da77a8a341ad8606
Nikolay <koddek@bk.ru>
2024-02-14 12:12:57 +0300

    initial commit

---
+++ Registration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registration
+//
+//  Created by KOДИ on 14.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
